222982db817a180a7d74d9731064af8aa18f6eee
Ekow <edonohue8@users.noreply.github.com>
2021-09-07 10:06:35 -0700

    updated About page with technical skills

--- src/pages/About.js
+++ src/pages/About.js
@@ -31,6 +31,20 @@ const About = () => {
 
                                     <p>Please see above for links to my portfolio and contact information.</p>
 
+                                    <hr></hr>
+
+                                    <p><b>Technical Skills:</b></p>
+
+                                    <p>Browser Based Technologies: HTML, CSS, JavaScript, jQuery, Responsive Design, Bootstrap, Handlebars, Local/Session Storage and IndexedDB, React.js</p>
+                                    <p>Other languages: C#</p>
+                                    <p>API Interaction: API, JSON, AJAX</p>
+                                    <p>Deployment/Command-Line Fundamentals: Heroku, Git, Github pages</p>
+                                    <p>Databases: MySQL, MongoDB</p>
+                                    <p>Server Side Development: Node.js, Express, User Authentication, PWAs, MERN Stack</p>
+                                    <p>Quality Assurance: Unit Testing, Functional Testing, Linting, Continuous Integration</p>
+                                    <p>Misc: Proven ability to learn new technologies and use that knowledge effectively | Knowledge of Agile Scrum Methodology | Knowledge of Software Development Life Cycle and DevOps | Typing speed of 95 WPM</p>
+
+
                                 </div>
                             </div>
                         </blockquote>
